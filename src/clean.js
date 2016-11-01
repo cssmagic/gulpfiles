1c2f3140d0eeaf5efbcac7a20feca5950ac29487
cssmagic <cssmagic.cn@gmail.com>
2016-11-01 11:52:09 +0800

    Update clean task.

--- src/clean.js
+++ src/clean.js
@@ -1,13 +1,13 @@
 'use strict'
 
-const path = require('path')
-const gulp = require('gulp')
-const del = require('del')
+module.exports = ({src, options}) => {
 
-module.exports = (globSrc, options) => {
+	const path = require('path')
+	const gulp = require('gulp')
+	const del = require('del')
 
 	gulp.task('clean', function () {
-		return del(globSrc, options).then(function (deletedFiles) {
+		return del(src, options).then(function (deletedFiles) {
 			const infoTitle = '[clean] deleted: '
 			if (deletedFiles.length) {
 				console.log(infoTitle)
